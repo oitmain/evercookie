301b42495e7719a131e272d542ea780b8f8f47e5
Patrick Yi <pyi@oitmain.com>
2016-11-15 01:08:03 +0000

    Add dtjava.js locally Remove goole analytics from index.html Reformat javascript Add option to disable cookie methods Skip checking disabled cookie methods, so script does not hang for 3 seconds Disable history, java, swf and silverlight by default Make etag header check case insensitive

--- php/evercookie_etag.php
+++ php/evercookie_etag.php
@@ -42,7 +42,10 @@ if (empty($_COOKIE[$cookie_name])) {
     }
 
     $headers = apache_request_headers();
-    if(isset($headers['If-None-Match'])) {
+    
+    $headers = array_change_key_case($headers, CASE_UPPER);
+    
+    if(isset($headers['IF-NONE-MATCH'])) {
         // extracting value from ETag presented format (which may be prepended by Weak validator modifier)
         $etag_value = preg_replace('|^(W/)?"(.+)"$|', '$2', $headers['If-None-Match']);
         header('HTTP/1.1 304 Not Modified');
